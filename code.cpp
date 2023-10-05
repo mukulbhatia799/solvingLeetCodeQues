466e40a97670915c3c1d2b5055750ee334a8d517
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-10-05 23:23:01 +0530

    print no. of 100s 50s 20s 1s notes of number n.

--- code.cpp
+++ code.cpp
@@ -1,31 +1,71 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int removeElement(vector<int>& vec, int val) {
-        int ptr2 = vec.size()-1, count = 0;
-        for(int i = 0; i < vec.size(); i++)
-        {
-            if(ptr2 < i) break;
-            if(vec[i] == val)
-            {
-                while(ptr2 > i)
-                {
-                    if(vec[ptr2] == val) ptr2--;
-                    else break;
-                }
-                swap(vec[ptr2], vec[i]);
-            }
-        }
-        for(int i = 0; i < vec.size(); i++)
-        {
-            if(vec[i] == val) break;
-            count++;
-        }
-        return count;
-    }
+int main(){
+    int n;
+    cin >> n;
 
-int main() {
-    
+    int count100 = 0, count50 = 0, count20 = 0, count1 = 0;
+    // while(n)
+    // {
+    //     if(n / 100 != 0)
+    //     {
+    //         count100 += n / 100;
+    //         n -= 100*(n/100);
+    //     }
+    //     else if(n / 50 != 0)
+    //     {
+    //         count50 += n / 50;
+    //         n -= 50*(n/50);
+    //     }
+    //     else if(n / 20 != 0)
+    //     {
+    //         count20 += n / 20;
+    //         n -= 20*(n/20);
+    //     }
+    //     else {
+    //         count1 += n;
+    //         n = 0;
+    //     }
+    // }
+
+    while(n)
+    {
+        int noofnotes = 0, token;
+        if(n / 100 != 0) token = 100;
+        else if(n / 50 != 0) token = 50;
+        else if(n / 20 != 0) token = 20;
+        else token = 1;
+        switch (token)
+        {
+            case 100:
+                noofnotes = n/100;
+                n -= 100*noofnotes;
+                cout << "There are " << noofnotes << " of 100." << endl;
+                break;
+            
+            case 50:
+                noofnotes = n/50;
+                n -= 50*(n/50);
+                cout << "There are " << noofnotes << " of 50." << endl;
+                break;
+            
+            case 20:
+                noofnotes = n/20;
+                n -= 20*(n/20);
+                cout << "There are " << noofnotes << " of 20." << endl;
+                break;
+
+            case 1:
+                noofnotes = n;
+                n = 0;
+                cout << "There are " << noofnotes << " of 1." << endl;
+                break;
+            
+            default:
+                break;
+        }
+    }
 
     return 0;
 }
