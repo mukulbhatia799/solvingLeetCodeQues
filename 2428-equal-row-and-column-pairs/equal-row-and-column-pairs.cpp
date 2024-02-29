7c2f69ae640bc1058b6e7dcf3e41f730d9b9eeb4
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-02-29 18:57:53 +0530

    Time: 119 ms (37.27%) | Memory: 26.2 MB (86.42%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        // time complexity:- O(n^3) because one for row, column and innermost loop is for comparision. So, in worst case, it can go upto O(n^3).
         int n = grid.size();
         int count = 0;
         for(int j = 0; j < n; j++) {
