276696c5dea8294e1c8fc8ae3976e3136aeb689b
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-03-29 20:03:56 +0530

    solved

--- FAANGQues/medium-hard-ques.cpp
+++ FAANGQues/medium-hard-ques.cpp
@@ -5,6 +5,7 @@ public:
         for(int i = 0; i < nums.size(); i++)
             ans.push_back(nums[nums[i]]);
         
+        // solved in leetcode...check there
         return ans;
     }
 };
