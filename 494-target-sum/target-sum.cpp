94876e6f4249713844870b81ee57e425cd653d2d
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-07-04 18:00:11 +0530

    Time: 216 ms (35.43%) | Memory: 59.1 MB (14.74%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int findAns(int n, vector<int>& nums, int target, int pos, int neg) {
+    int findAns(int n, vector<int>& nums, int target, int pos, int neg, map<pair<int, int>, int> &dp) {
         if(n < 0) {
             return (pos+neg == target);
         }
 
+        if(dp.find({pos, neg}) != dp.end()) return dp[{pos, neg}];
+
         // positive value;
-        int posSum = findAns(n-1, nums, target, pos+nums[n], neg);
+        int posSum = findAns(n-1, nums, target, pos+nums[n], neg, dp);
 
         // negative value;
-        int negSum = findAns(n-1, nums, target, pos, neg-nums[n]);
+        int negSum = findAns(n-1, nums, target, pos, neg-nums[n], dp);
 
-        return (posSum + negSum);
+        return dp[{pos, neg}] = (posSum + negSum);
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
+        // using memoization
         int n = nums.size();
 
-        return findAns(n-1, nums, target, 0, 0);
+        map<pair<int, int>, int> dp;
+
+        return findAns(n-1, nums, target, 0, 0, dp);
     }
 };
