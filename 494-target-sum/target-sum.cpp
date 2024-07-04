d2a9231e93f8c2da8186e4ff7713f7802926e9b3
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-07-04 19:02:53 +0530

    Time: 14 ms (51.48%) | Memory: 24 MB (28.05%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int findAns(int n, vector<int>& nums, int target, int pos, int neg, map<pair<int, int>, int> &dp) {
+    int findAns(int n, vector<int>& nums, int target, int pos, int neg, int sum, vector<vector<int>> &dp) {
         if(n < 0) {
             return (pos+neg == target);
         }
 
-        if(dp.find({pos, neg}) != dp.end()) return dp[{pos, neg}];
+        int col = (pos+neg <= 0) ? abs(pos+neg) : sum+abs(pos+neg);
+        if(dp[n][col] != -1) return dp[n][col];
 
         // positive value;
-        int posSum = findAns(n-1, nums, target, pos+nums[n], neg, dp);
+        int posSum = findAns(n-1, nums, target, pos+nums[n], neg, sum, dp);
 
         // negative value;
-        int negSum = findAns(n-1, nums, target, pos, neg-nums[n], dp);
+        int negSum = findAns(n-1, nums, target, pos, neg-nums[n], sum, dp);
 
-        return dp[{pos, neg}] = (posSum + negSum);
+        return dp[n][col] = (posSum + negSum);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         // using memoization
         int n = nums.size();
 
-        map<pair<int, int>, int> dp;
+        int sum = 0;
+        for(int i = 0; i < n; i++) sum += nums[i];
 
-        return findAns(n-1, nums, target, 0, 0, dp);
+        vector<vector<int>> dp(n, vector<int> ((sum+1)*2, -1));
+
+        return findAns(n-1, nums, target, 0, 0, sum, dp);
     }
 };
