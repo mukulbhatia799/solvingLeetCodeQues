ce4fe1c3ef38db1b803c6c186090e0a96e0fdc53
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-08-20 17:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (26.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,8 +12,15 @@ public:
         return dp[n] = step1 + step2;
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
+        if(n <= 2) return n;
+        vector<int> dp(n+1, 0);
 
-        return findAns(n, dp);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
