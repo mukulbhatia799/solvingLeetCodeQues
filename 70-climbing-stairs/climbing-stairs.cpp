59c8a3a8c8f90a420d636494c7f47bfdc3735b1a
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-08-20 17:37:37 +0530

    Time: 3 ms (29.50%) | Memory: 7.6 MB (26.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int findAns(int n, vector<int> &dp) {
-        if(n <= 0) return (n==0)?1:0;
+    int findAns(int n, vector<int>& dp) {
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+
         if(dp[n] != -1) return dp[n];
-        return dp[n] = findAns(n-1, dp) + findAns(n-2, dp);
+
+        int step1 = findAns(n-1, dp);
+        int step2 = findAns(n-2, dp);
+
+        return dp[n] = step1 + step2;
     }
     int climbStairs(int n) {
-        // using memoization
         vector<int> dp(n+1, -1);
+
         return findAns(n, dp);
     }
 };
