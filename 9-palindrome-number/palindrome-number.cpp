ef5baa3b294f34e1622564648adbcdd2c4db9192
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-09-03 14:44:54 +0530

    Time: 12 ms (34.05%) | Memory: 9.3 MB (24.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,7 +3,12 @@ public:
     bool isPalindrome(int x) {
         if(x < 0) return false;
 
-        string s = to_string(x);
+        string s = "";
+        while(x) {
+            char mod = (x % 10) + '0';
+            s.push_back(mod);
+            x /= 10;
+        }
         int i = 0, j = s.size()-1;
         while(i < j) {
             if(s[i++] != s[j--]) return false;
