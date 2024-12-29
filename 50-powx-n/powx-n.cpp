18852fac453bae1168877f85afe6ac9548ddf90c
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2024-12-29 14:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (10.36%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        double ans = 1;
+        int m = n;
+
+        while(n) {
+            if(n & 1) ans *= x;
+            x *= x;
+            n /= 2;
+        }
+
+        if(m < 0) ans = 1 / ans;
+        return ans;
     }
 };
