e53bf0bbb34a3ee78ef84da898547682c2ef5b5c
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-03-26 12:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.91%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         for(int i = n-1; i > 0; i--) {
             if(nums[i] > nums[i-1]) {
-                sort(nums.begin()+i, nums.end());
+                reverse(nums.begin()+i, nums.end());
                 for(int j = i; j < n; j++) {
                     if(nums[i-1] < nums[j]) {
                         swap(nums[i-1], nums[j]);
